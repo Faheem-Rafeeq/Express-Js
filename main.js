ee057bb09dc462ad57c51ec3bd72b3de6576bcb7
Faheem-Rafeeq <imfaheemrafeeq78@gmail.com>
2025-06-20 21:12:03 +0500

    checking get request

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const express = require('express')
+import express from 'express';
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     console.log('hey its a get quest')
   res.send('hey its a get request!')
+})
+
+app.get('/password', (req, res) => {
+    console.log('hey its a password get request')
+  res.send('hey its a password  get request!')
 })
 
 app.put('/', (req, res) => {
